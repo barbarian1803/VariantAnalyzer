4b23fca38367e69982aab6ad0064942d1602f48e
barbarian1803 <bharata180@gmail.com>
2017-03-17 13:11:56 +0900

    Read VCF file test

--- src/variantanalyzer/VariantFile.java
+++ src/variantanalyzer/VariantFile.java
@@ -1,6 +1,63 @@
 package variantanalyzer;
 
+import java.util.ArrayList;
+
 public class VariantFile {
-    private String[] metadata;
-    private VariantResult[] variantResult;
+    private ArrayList<String> metadata;
+    private ArrayList<String> colNames;
+    private ArrayList<VariantResult> variantResult;
+
+    public VariantFile() {
+        this.metadata = new ArrayList();
+        this.colNames = new ArrayList();
+        this.variantResult = new ArrayList();
+    }
+
+    public void addMetadata(String data){
+        this.metadata.add(data);
+    }
+    
+    public void printMetada(){
+        for(String metadata: this.metadata){
+            System.out.println(metadata);
+        }
+    }
+    
+    public void addColNames(String data){
+        this.colNames.add(data);
+    }
+    
+    public void printColNames(){
+        System.out.println(this.colNames.toString());
+    }
+    
+    public void addVariantResult(VariantResult entry){
+        this.variantResult.add(entry);
+    }
+    
+    public ArrayList<String> getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(ArrayList<String> metadata) {
+        this.metadata = metadata;
+    }
+
+    public ArrayList<String> getColNames() {
+        return colNames;
+    }
+
+    public void setColNames(ArrayList<String> colNames) {
+        this.colNames = colNames;
+    }
+
+    public ArrayList<VariantResult> getVariantResult() {
+        return variantResult;
+    }
+
+    public void setVariantResult(ArrayList<VariantResult> variantResult) {
+        this.variantResult = variantResult;
+    }
+    
+    
 }
--- src/variantanalyzer/VariantResult.java
+++ src/variantanalyzer/VariantResult.java
@@ -1,11 +1,30 @@
 package variantanalyzer;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class VariantResult {
-    private int chrom;
-    private int pos;
-    private String id;
-    private String ref;
-    private String alt;
+    private Map<String,String> item;
+
+    public VariantResult() {
+        this.item = new HashMap();
+    }
+
+    public void addItem(String key, String value){
+        this.item.put(key, value);
+    }
+    
+    public Map<String, String> getItem() {
+        return item;
+    }
+
+    public void setItem(Map<String, String> item) {
+        this.item = item;
+    }
+    
+    public void printVariant(){
+        System.out.println(this.item.toString());
+    }
     
 }
