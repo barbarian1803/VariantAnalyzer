81826003da2c589ab5115891e4f380be48797401
bharata <bharata@bharata-desktop>
2017-03-31 13:43:14 +0900

    Transcript fasta file extract only for chromosome 1

--- src/variantanalyzer/CDNAFastaParser.java
+++ src/variantanalyzer/CDNAFastaParser.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package variantanalyzer;
 
 import java.io.BufferedReader;
@@ -23,8 +18,7 @@ import java.util.logging.Logger;
 public class CDNAFastaParser {
     
     public static void main(String[] args){
-        CDNAFastaParser obj = new CDNAFastaParser();
-        obj.FastaSlicer();
+        new CDNAFastaParser().FastaSlicer();
     }
     
     public void FastaSlicer() {
