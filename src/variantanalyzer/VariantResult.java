0e8c97025e405efcde7ab7c2eec5a820a9bf724a
bharata <bharata@bharata-desktop>
2017-04-04 09:55:15 +0900

    move coordinate subclass to public class

--- src/variantanalyzer/VariantResult.java
+++ src/variantanalyzer/VariantResult.java
@@ -55,35 +55,4 @@ public class VariantResult {
     public void addCoordinate(String transcript, int exon){
         this.coordinates.add(new Coordinate(transcript, exon));
     }
-    
-    private class Coordinate{
-        String transcriptID;
-        int exonID;
-
-        public Coordinate(String transcriptID, int exonID) {
-            this.transcriptID = transcriptID;
-            this.exonID = exonID;
-        }
-
-        public String getTranscriptID() {
-            return transcriptID;
-        }
-
-        public void setTranscriptID(String transcriptID) {
-            this.transcriptID = transcriptID;
-        }
-
-        public int getExonID() {
-            return exonID;
-        }
-
-        public void setExonID(int exonID) {
-            this.exonID = exonID;
-        }
-        
-        @Override
-        public String toString(){
-            return "["+transcriptID+","+exonID+"]";
-        }
-    }
 }
