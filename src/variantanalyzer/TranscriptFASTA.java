d5473e19f1391582ed63d94b66a72e1b292ef499
bharata <bharata@bharata-desktop>
2017-04-04 09:56:56 +0900

    add strand attribute and printData function

--- src/variantanalyzer/TranscriptFASTA.java
+++ src/variantanalyzer/TranscriptFASTA.java
@@ -10,19 +10,29 @@ import java.util.Map;
 public class TranscriptFASTA {
     private String sequence;
     private String transcriptID;
+    private int strand;
     private Map<Integer,GTFEntry> transcriptExons;
     
+    
     public TranscriptFASTA(String metadata){
-        this.transcriptID = this.parseMetadata(metadata);
+        this.transcriptID = this.parseTranscriptID(metadata);
+        this.strand = this.parseStrand(metadata);
         this.sequence = "";
         transcriptExons = new HashMap();
     }
     
-    private String parseMetadata(String metadata){
+    private String parseTranscriptID(String metadata){
         String[] parsedMetadata = metadata.substring(1).split(" ");
         //first element: transcript id
         String[] parsedData = parsedMetadata[0].split("\\.");
         return parsedData[0];
+    }
+    
+    private int parseStrand(String metadata){
+        String[] parsedMetadata = metadata.substring(1).split(" ");
+        //first element: transcript id
+        String[] parsedData = parsedMetadata[2].split(":");
+        return Integer.parseInt(parsedData[5]);
     }
     
     public void appendSequence(String sequence){
@@ -47,5 +57,16 @@ public class TranscriptFASTA {
     
     public void addExon(int key, GTFEntry exon){
         this.transcriptExons.put(key,exon);
+    }
+    
+    public void printData(){
+        System.out.println(this.transcriptID);
+        System.out.println("Strand: "+this.strand);
+        System.out.println(this.sequence);
+        Object[] exonsNumber = this.transcriptExons.keySet().toArray();
+        for(Object i : exonsNumber){
+            System.out.print(i+" ");
+            this.transcriptExons.get((int)i).printEntry();
+        }
     }
 }
