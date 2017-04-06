6fd782a8f25213ea967707bdd2d3619384b5e9e8
bharata <bharata@bharata-desktop>
2017-04-06 19:54:13 +0900

    - create function to return all possible allele in the samples - create function to return genotype for given sample

--- src/variantanalyzer/VariantResult.java
+++ src/variantanalyzer/VariantResult.java
@@ -2,8 +2,10 @@ package variantanalyzer;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 
 public class VariantResult {
@@ -54,5 +56,31 @@ public class VariantResult {
     
     public void addCoordinate(String transcript, int exon){
         this.coordinates.add(new Coordinate(transcript, exon));
+    }
+    
+    public String[] returnAlleleList(){
+        //index 0 is reference while 1,2,3,etc are the alternatives
+        String[] alt = this.getColValues("ALT").split(",");
+        String[] retval = new String[alt.length+1];
+        retval[0]=this.getColValues("REF");
+        for(int i=0;i<alt.length;i++){
+            retval[i+1]=alt[i];
+        }
+        return retval;
+    }
+        
+    public Set returnGenotypeFromSample(String sample){
+        String alleleCol = this.getColValues(sample).split(":")[0];
+        
+        String[] alleleID = alleleCol.split("/|\\|");
+        Set<String> retVal = new HashSet();
+        for(String i:alleleID){
+            if(i.equalsIgnoreCase(".")){
+                retVal.add("0");
+            }else{
+                retVal.add(i);
+            }
+        }
+        return retVal;
     }
 }
