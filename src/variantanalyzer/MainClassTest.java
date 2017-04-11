0b5d7650590e491324c525854f6678d22dc836ec
bharata <bharata@bharata-desktop>
2017-04-11 19:16:45 +0900

    add some test

--- src/variantanalyzer/MainClassTest.java
+++ src/variantanalyzer/MainClassTest.java
@@ -1,12 +1,6 @@
 package variantanalyzer;
 
-import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.io.Writer;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -27,16 +21,15 @@ public class MainClassTest {
         //Assign exon list to transcript sequence
         TranscriptFASTAFile transcriptWithExons = TranscriptFASTAParser.AssignExonsPerTranscript(transcripts, gtf,threadNumber);
         
-        //transcriptWithExons.getTranscript("ENST00000335137").printData();
-        
-        //Assign variant location to corresponding annotation
+       
+//        Assign variant location to corresponding annotation
         VariantFile vcfOut = VCFParser.AssignVariantLocation(vcf, gtf, threadNumber);
         
         
         try {
             VCFParser.TranscriptConsensusSequence(vcfOut, transcriptWithExons, "33","data/consensus.fa");
         } catch (FileNotFoundException ex) {
-            Logger.getLogger(MainClassTest.class.getName()).log(Level.SEVERE, null, ex);
+            
         }
         
     }
